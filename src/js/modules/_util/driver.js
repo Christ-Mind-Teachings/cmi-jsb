933e8892e711a4041a9d7b2565ba93c76c31737b
Rick Mercer <rmercer33@gmail.com>
2019-01-14 20:26:30 +0800

    [skip ci] improved driver.js

--- src/js/modules/_util/driver.js
+++ src/js/modules/_util/driver.js
@@ -1,71 +1,212 @@
 import Driver from "driver.js";
 
+const cmiPageTitle = {
+  element: "#source-homepage",
+  popover: {
+    title: "Title",
+    description: "This is the homepage for <em>The Impersonal Life</em> by Joseph Benner in the Christ Mind Library.<br><br>Click on the book cover below to see the table of contents.",
+    position: "bottom"
+  }
+};
+
+const cmiPageBanner = {
+  element: "#masthead-title",
+  popover: {
+    title: "Navigation and Features",
+    description: "<em>The Impersonal Life</em> is part of the Library of Christ Mind Teachings. On every page you can click on this banner to navigate to the Library's main page and see all available teachings.",
+    position: "bottom"
+  }
+};
+
+const theImpersonalLife = {
+  element: "[data-book='til']",
+  popover: {
+    title: "The Impersonal Life",
+    description: "By Joseph S. Benner, originally published in 1914. Audio is available and offered by Barry Peterson.",
+    position: "top"
+  }
+};
+
+const pageMenu = {
+  element: "#page-menu",
+  popover: {
+    title: "The Menu",
+    description: "This is the page menu, it will stick to the top when the page is scrolled (when the tour is over) so it is always available. The menu on other pages is similar but may contain additional features.<br/><br/>A brief description of each menu option follows.",
+    position: "bottom"
+  }
+};
+
+const pageMenuBookmarkItem = {
+  element: ".bookmark-modal-open",
+  popover: {
+    title: "List Bookmarks",
+    description: "Display a list of bookmarks you have created and optionally filter by topic. You can quickly jump to any bookmark. Learn more about bookmarks in the documentation.",
+    position: "bottom"
+  }
+};
+
+const pageMenuSearchItem = {
+  element: ".search-modal-open",
+  popover: {
+    title: "Search Through All Books",
+    description: "Find topics of interest by searching through all ACIM books.",
+    position: "bottom"
+  }
+};
+
+const pageMenuHelpItem = {
+  element: "#help-menu",
+  popover: {
+    title: "Get Help and Learn About",
+    description: "Learn about the Library and using the features of the site.",
+    position: "bottom"
+  }
+};
+
+const pageMenuLoginItem = {
+  element: ".login-menu-option",
+  popover: {
+    title: "Sign In/Sign Out",
+    description: "Create an account and sign in to the site. It's free and allows you to create bookmarks that you can share via Facebook and keep synchronized between devices.",
+    position: "left"
+  }
+};
+
+const pageMenuTextContents = {
+  element: "[data-book='til']",
+  popover: {
+    title: "Display Table of Contents",
+    description: "Click on any image to display and navigate to the volume contents.",
+    position: "left"
+  }
+};
+
+const cmiTranscriptBanner = {
+  element: "#masthead-title",
+  popover: {
+    title: "Library of Christ Mind Teachings",
+    description: "This page is part of the Teachings of Christ Mind Library. Click this link to navigate to the Library's Home page.",
+    position: "bottom"
+  }
+};
+
+const cmiTranscriptSourceTitle = {
+  element: "#src-title",
+  popover: {
+    title: "A Course In Miracles",
+    description: "This page is part of A Course In Miracles. Click this link to navigate to the ACIM Home page.",
+    position: "bottom"
+  }
+};
+
+const cmiTranscriptBookTitle = {
+  element: "#book-title",
+  popover: {
+    title: "Page Title",
+    description: "This identifies the book and section or lesson of the content on this page.",
+    position: "bottom"
+  }
+};
+
+const transcriptMenuBookmarkItem = {
+  element: "#bookmark-dropdown-menu",
+  popover: {
+    title: "Bookmarks",
+    description: "You can create a bookmark from highlighted text and associate the bookmark with one or more categories. Learn more about bookmarks by reading the documentation.",
+    position: "right"
+  }
+};
+
+const transcriptMenuSearchItem = {
+  element: ".search-modal-open",
+  popover: {
+    title: "Search Through All Books",
+    description: "Find topics of interest by searching through all ACIM books.",
+    position: "bottom"
+  }
+};
+
+const transcriptMenuAudioItem = {
+  element: ".audio-player-toggle",
+  popover: {
+    title: "Listen to the Audio",
+    description: "Click the speaker icon to display the audio player and listen along as you read.",
+    position: "bottom"
+  }
+};
+
+const transcriptMenuParagraphMarkerItem = {
+  element: ".toggle-paragraph-markers",
+  popover: {
+    title: "Show/Hide Paragraph Markers",
+    description: "Show or hide the markers that preceed each paragraph.",
+    position: "bottom"
+  }
+};
+
+const transcriptMenuPageTopItem = {
+  element: ".top-of-page",
+  popover: {
+    title: "Go To Top of Page",
+    description: "Quickly jump to the top of the page.",
+    position: "bottom"
+  }
+};
+
+const transcriptMenuContentsItem = {
+  element: "#contents-modal-open",
+  popover: {
+    title: "Table of Contents",
+    description: "View the table of contents.",
+    position: "bottom"
+  }
+};
+
+const transcriptMenuPreviousPageItem = {
+  element: ".previous-page",
+  popover: {
+    title: "Previous Page",
+    description: "Go to the previous page. This will be disabled when the first page is displayed.",
+    position: "bottom"
+  }
+};
+
+const transcriptMenuNextPageItem = {
+  element: ".next-page",
+  popover: {
+    title: "Next Page",
+    description: "Go to the next page. This will be disabled when the last page is displayed.",
+    position: "bottom"
+  }
+};
+
+const transcriptMenuHelpItem = {
+  element: "#about-dropdown-menu",
+  popover: {
+    title: "Get Help",
+    description: "Learn how to use features of the Library.",
+    position: "bottom"
+  }
+};
+
+const transcriptMenuLoginItem = {
+  element: ".login-menu-option",
+  popover: {
+    title: "Sign In/Sign Out",
+    description: "Create an account and sign in or sign out. When you sign in, bookmarks you create will be available on all devices you use to access the library.",
+    position: "bottom"
+  }
+};
+
 export function pageDriver() {
   const driver = new Driver({
     allowClose: false,
-    opacity: 0.5,
-    onHighlightStarted: (el) => {
-      console.log("highlighting %o", el);
-    }
+    opacity: 0.5
   });
 
   driver.defineSteps([
-    {
-      element: "#source-homepage",
-      popover: {
-        title: "The Way of Mastery",
-        description: "This is the homepage for all of <em>The Way of Mastery</em> teachings in the Library. Each of the volumes below contain chapters and some included questions too. And most contain the original audio so you can read along as you listen.<br><br>Click on the image to see the table of contents. The Way of Mastery teachings include&hellip;",
-        position: "bottom"
-      }
-    },
-    {
-      element: "[data-book='woh']",
-      popover: {
-        title: "The Way of the Heart",
-        description: "The first of three years of monthly messages that present the fundamental ideas and exercises that form a pathway of awakening.",
-        position: "top"
-      }
-    },
-    {
-      element: "[data-book='wot']",
-      popover: {
-        title: "The Way of Transformation",
-        description: "The second year of monthly messages that presents a transformative way of life.",
-        position: "top"
-      }
-    },
-    {
-      element: "[data-book='wok']",
-      popover: {
-        title: "The Way of Knowing",
-        description: "The final year of monthly messages that culminates <em>The Way of Mastery</em>.",
-        position: "top"
-      }
-    },
-    {
-      element: "[data-book='tjl']",
-      popover: {
-        title: "The Jeshua Letters",
-        description: "Jayem's account of his introduction to Jeshua.",
-        position: "top"
-      }
-    },
-    {
-      element: "[data-book='wos']",
-      popover: {
-        title: "The Way of the Servant",
-        description: "A poetic description of the journey of awakening taken from inception to recognition of self as the servant - one who walks this world awake.",
-        position: "top"
-      }
-    },
-    {
-      element: "[data-book='early']",
-      popover: {
-        title: "The Early Years",
-        description: "A collection of messages given before the Way of the Heart.",
-        position: "top"
-      }
-    }
+    cmiPageTitle,
+    theImpersonalLife
   ]);
 
   driver.start();
@@ -74,70 +215,24 @@ export function pageDriver() {
 export function pageNavigationDriver() {
   const driver = new Driver({
     allowClose: false,
-    opacity: 0.5,
-    onReset: () => {
-      $("#bookmark-dropdown-menu").dropdown("hide");
-    }
+    opacity: 0.5
   });
-  driver.defineSteps([
-    {
-      element: "#masthead-title",
-      popover: {
-        title: "Navigation and Features",
-        description: "The Way of Mastery is part of the Library of Christ Mind Teachings. On every page you can click here to display the Library's main page to see all available teachings.",
-        position: "bottom"
-      }
-    },
-    {
-      element: "#page-menu",
-      popover: {
-        title: "The Menu",
-        description: "This is the page menu, it will stick to the top when the page is scrolled (when the tour is over) so it is always available. The menu on other pages is similar but may contain additional features.",
-        position: "bottom"
-      }
-    },
-    {
-      element: ".bookmark-modal-open",
-      popover: {
-        title: "List Bookmarks",
-        description: "Display a list of bookmarks you have created and optionally filter by topic. You can quickly jump to any bookmark. Learn more about bookmarks in the documentation.",
-        position: "bottom"
-      }
-    },
-    {
-      element: ".search-modal-open",
-      popover: {
-        title: "Search Through All Books",
-        description: "Find topics of interest by searching through all Way of Mastery books.",
-        position: "bottom"
-      }
-    },
-    {
-      element: "#help-menu",
-      popover: {
-        title: "Get Help and Learn About",
-        description: "Learn about the teaching and using the features of the site.",
-        position: "bottom"
-      }
-    },
-    {
-      element: ".login-menu-option",
-      popover: {
-        title: "Sign In/Sign Out",
-        description: "Create an account and sign in to the site. It's free and allows you to create bookmarks that you can share via Facebook and keep synchronized between devices.",
-        position: "left"
-      }
-    },
-    {
-      element: "[data-book='wot']",
-      popover: {
-        title: "Display Table of Contents",
-        description: "Click on any image to display and navigate to the volume contents.",
-        position: "left"
-      }
-    }
-  ]);
 
+  let pageNavigationDriverSteps = [];
+
+  pageNavigationDriverSteps.push(cmiPageBanner);
+  pageNavigationDriverSteps.push(pageMenu);
+  pageNavigationDriverSteps.push(pageMenuBookmarkItem);
+
+  if ($(".search-modal-open").length > 0) {
+    pageNavigationDriverSteps.push(pageMenuSearchItem);
+  }
+
+  pageNavigationDriverSteps.push(pageMenuHelpItem);
+  pageNavigationDriverSteps.push(pageMenuLoginItem);
+  pageNavigationDriverSteps.push(pageMenuTextContents);
+
+  driver.defineSteps(pageNavigationDriverSteps);
   driver.start();
 }
 
@@ -145,106 +240,36 @@ export function transcriptDriver() {
   const driver = new Driver({
     allowClose: false,
     opacity: 0.5
-    /*
-    onReset: () => {
-      $("#bookmark-dropdown-menu").dropdown("hide");
-    }
-    */
   });
 
-  driver.defineSteps([
-    {
-      element: "#masthead-title",
-      popover: {
-        title: "Library of Christ Mind Teachings",
-        description: "This page is part of the Teachings of Christ Mind Library. Click this link to navigate to the Library's Home page.",
-        position: "bottom"
-      }
-    },
-    {
-      element: "#src-title",
-      popover: {
-        title: "Way of Mastery",
-        description: "This page comes from the Way of Mastery. Click this link to navigate to the Home page of the Way of Mastery.",
-        position: "bottom"
-      }
-    },
-    {
-      element: "#book-title",
-      popover: {
-        title: "Book Title",
-        description: "This identifies the book and chapter of the content on this page.",
-        position: "bottom"
-      }
-    },
-    {
-      element: "#bookmark-dropdown-menu",
-      popover: {
-        title: "Bookmarks",
-        description: "You can create a bookmark from highlighted text and associate the bookmark with one or more categories. Learn more about bookmarks by reading the documentation.",
-        position: "right"
-      }
-    },
-    {
-      element: ".search-modal-open",
-      popover: {
-        title: "Search Through All Books",
-        description: "Find topics of interest by searching through all Way of Mastery books.",
-        position: "bottom"
-      }
-    },
-    {
-      element: ".audio-player-toggle",
-      popover: {
-        title: "Listen to the Audio",
-        description: "Click the speaker icon to display the audio player and listen along as you read.",
-        position: "bottom"
-      }
-    },
-    {
-      element: ".toggle-paragraph-markers",
-      popover: {
-        title: "Show/Hide Paragraph Markers",
-        description: "Show or hide the markers that preceed each paragraph.",
-        position: "bottom"
-      }
-    },
-    {
-      element: ".top-of-page",
-      popover: {
-        title: "Go To Top of Page",
-        description: "Quickly jump to the top of the page.",
-        position: "bottom"
-      }
-    },
-    {
-      element: "#contents-modal-open",
-      popover: {
-        title: "Table of Contents",
-        description: "View the table of contents.",
-        position: "bottom"
-      }
-    },
-    {
-      element: "#about-dropdown-menu",
-      popover: {
-        title: "Get Help",
-        description: "Learn how to use features of the Library.",
-        position: "bottom"
-      }
-    },
-    {
-      element: ".login-menu-option",
-      popover: {
-        title: "Sign In/Sign Out",
-        description: "Create an account and sign in or sign out. When you sign in, bookmarks you create will be available on all devices you use to access the library.",
-        position: "bottom"
-      }
-    }
-  ]);
+  let transcriptDriverSteps = [];
 
-  //show bookmark menu
-  //$("#bookmark-dropdown-menu").dropdown("show");
+  transcriptDriverSteps.push(cmiTranscriptBanner);
+  transcriptDriverSteps.push(cmiTranscriptSourceTitle);
+  transcriptDriverSteps.push(cmiTranscriptBookTitle);
+  transcriptDriverSteps.push(transcriptMenuBookmarkItem);
+
+  if ($(".search-modal-open").length > 0) {
+    transcriptDriverSteps.push(transcriptMenuSearchItem);
+  }
+
+  if (!$(".audio-player-toggle").hasClass("hide")) {
+    transcriptDriverSteps.push(transcriptMenuAudioItem);
+  }
+
+  transcriptDriverSteps.push(transcriptMenuParagraphMarkerItem);
+  transcriptDriverSteps.push(transcriptMenuPageTopItem);
+
+  if ($("#contents-modal-open").length > 0) {
+    transcriptDriverSteps.push(transcriptMenuContentsItem);
+    transcriptDriverSteps.push(transcriptMenuPreviousPageItem);
+    transcriptDriverSteps.push(transcriptMenuNextPageItem);
+  }
+
+  transcriptDriverSteps.push(transcriptMenuHelpItem);
+  transcriptDriverSteps.push(transcriptMenuLoginItem);
+
+  driver.defineSteps(transcriptDriverSteps);
   driver.start();
 }
 
